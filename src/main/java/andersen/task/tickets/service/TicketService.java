ab486bd429f9ce4c6ee181b20bf0f9410e461f02
KorolER <elizavetakorol2005@gmail.com>
2024-10-31 12:05:19 +0300

    Add constructor with tickets

--- src/main/java/andersen/task/tickets/service/TicketService.java
+++ src/main/java/andersen/task/tickets/service/TicketService.java
@@ -1,6 +1,8 @@
 package andersen.task.tickets.service;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
 import java.util.List;
 import java.util.Set;
 
@@ -12,7 +14,22 @@ import jakarta.validation.ConstraintViolationException;
 import jakarta.validation.Validation;
 
 public class TicketService {
-	List<Ticket> tickets = new ArrayList<>();
+	List<Ticket> tickets;
+	
+	public TicketService()
+	{
+		tickets = new ArrayList<>(Arrays.asList(
+				new Ticket("4 Hall", 345, new Calendar.Builder().setDate(2025, 0, 23).setTimeOfDay(19, 00, 0).build()),
+				new Ticket("Main hall", 231, new Calendar.Builder().setDate(2024, 4, 29).setTimeOfDay(15, 00, 0).build(), false, 'A', 45.6056374),
+				new Ticket("AAAA", 231, new Calendar.Builder().setDate(2025, 1, 1).setTimeOfDay(13, 00, 0).build(), false, 'C', 47.34),
+				new Ticket("bbbb", 865, new Calendar.Builder().setDate(2025, 1, 1).setTimeOfDay(13, 00, 0).build(), false, 'C', 45.78),
+				new Ticket("CCC", 345, new Calendar.Builder().setDate(2025, 1, 1).setTimeOfDay(13, 00, 0).build(), false, 'C', 47.34),
+				new Ticket("CCC", 423, new Calendar.Builder().setDate(2025, 1, 1).setTimeOfDay(13, 00, 0).build(), false, 'C', 47.34),
+				new Ticket("CCC", 345, new Calendar.Builder().setDate(2027, 1, 1).setTimeOfDay(13, 00, 0).build(), false, 'C', 234.53),
+				new Ticket("CCC", 345, new Calendar.Builder().setDate(2025, 1, 1).setTimeOfDay(13, 00, 0).build(), false, 'C', 14.34214),
+				new Ticket("CCC", 352, new Calendar.Builder().setDate(2024, 11, 11).setTimeOfDay(13, 00, 0).build(), false, 'C', 47.34),
+				new Ticket("CCC", 526, new Calendar.Builder().setDate(2025, 1, 1).setTimeOfDay(13, 00, 0).build(), false, 'C', 47.34)));
+	}
 	
 	public Ticket addTicket(Ticket ticket)
 	{
