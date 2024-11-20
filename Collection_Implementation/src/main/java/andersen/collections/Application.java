b0a5662bb24c0de2dcd23cbca84b868fb8cfb966
KorolER <elizavetakorol2005@gmail.com>
2024-11-20 10:17:37 +0300

    Implement put(int, T) from ArrayListImpl

--- Collection_Implementation/src/main/java/andersen/collections/Application.java
+++ Collection_Implementation/src/main/java/andersen/collections/Application.java
@@ -1,10 +1,18 @@
 package andersen.collections;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+
+import andersen.collections.collection.IArrayList;
+import andersen.collections.collection.impl.ArrayListImpl;
 
 public class Application {
 	public static void main(String[] args) {
-		List <Integer> list = new ArrayList <>();
+		ArrayListImpl<Integer> arr = new ArrayListImpl<>();
+		arr.put(2).put(4).put(5).put(6);
+		for (int i = 0; i < arr.getSize(); i++) {
+			System.out.print(arr.get(i) + " ");
+		}
 	}
 }
--- Collection_Implementation/src/main/java/andersen/collections/collection/impl/ArrayListImpl.java
+++ Collection_Implementation/src/main/java/andersen/collections/collection/impl/ArrayListImpl.java
@@ -1,26 +1,33 @@
 package andersen.collections.collection.impl;
 
 import andersen.collections.collection.IArrayList;
+import lombok.Getter;
 
 public class ArrayListImpl<T> implements IArrayList<T> {
 	private final int DEFAULT_CAPACITY = 10;
 	/**
-	 * Defines the grows of {@link #capacity}
+	 * Defines the grows/shrink of {@link #capacity}
 	 */
 	private final byte RESIZE_FACTOR = 2;
 
 	T array[];
-	int size;
-	int capacity;
+	@Getter
+	private int size;
+	private int capacity;
 
 	public ArrayListImpl() {
 		this.size = 0;
-		this.capacity = 0;
+		this.capacity = DEFAULT_CAPACITY;
 		this.array = (T[]) new Object[capacity];
 	}
 
+	/**
+	 * Add @param element to the end of a list
+	 * 
+	 * @return current object to allow chaining
+	 */
 	@Override
-	public void put(T element) {
+	public ArrayListImpl<T> put(T element) {
 		if (requiresResizing()) {
 			capacity *= RESIZE_FACTOR;
 			T[] bufArray = (T[]) new Object[capacity];
@@ -30,25 +37,46 @@ public class ArrayListImpl<T> implements IArrayList<T> {
 			array = bufArray;
 		}
 		array[size++] = element;
+		return this;
 	}
-
 
 	@Override
 	public T get(int index) {
-		// TODO Auto-generated method stub
-		return null;
+		return array[index];
 	}
 
 	@Override
 	public void put(int index, T element) {
-		// TODO Auto-generated method stub
-
+		if (requiresResizing()) {
+			capacity *= RESIZE_FACTOR;
+			T[] bufArray = (T[]) new Object[capacity];
+			for (int i = 0; i < index; i++) {
+				bufArray[i] = array[i];
+			}
+			for (int i = index; i < size; i++) {
+				bufArray[i + 1] = array[i];
+			}
+			bufArray[index] = element;
+			array = bufArray;
+		} else {
+			for (int i = index; i < size; i++) {
+				array[i + 1] = array[i];
+			}
+			array[index] = element;
+		}
 	}
 
+	// REQUIRES REFACTORING //
 	private boolean requiresResizing() {
-		if ((size < capacity/RESIZE_FACTOR && capacity > DEFAULT_CAPACITY) || size == capacity) {
+		if ((size < capacity / RESIZE_FACTOR && capacity > DEFAULT_CAPACITY) || size == capacity) {
 			return true;
+		}
 		return false;
 	}
 
+	@Override
+	public boolean delete(int index) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 }
