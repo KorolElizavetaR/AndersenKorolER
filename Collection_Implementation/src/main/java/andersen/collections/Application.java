21bcd39831023f4126806c581b6462c97cb4968f
KorolER <elizavetakorol2005@gmail.com>
2024-11-20 19:23:04 +0300

    Minor refactoring (view desc)
    Uncommented code; Changed access modifier of T array[] in ArrayListImpl;
    add negative value check in put(int, T) method from ArrayListImpl.

--- Collection_Implementation/src/main/java/andersen/collections/Application.java
+++ Collection_Implementation/src/main/java/andersen/collections/Application.java
@@ -1,15 +1,16 @@
 package andersen.collections;
 
+import andersen.collections.collection.impl.ArrayListImpl;
 import andersen.collections.collection.impl.HashSetImpl;
 
 public class Application {
 	public static void main(String[] args) {
-//		ArrayListImpl<Integer> arr = new ArrayListImpl<>();
-//		arr.put(2).put(4).put(5).put(6).put(2, 10);
-//		arr.delete(1);
-//		for (int i = 0; i < arr.getSize(); i++) {
-//			System.out.print(arr.get(i) + " ");
-//		}
+		ArrayListImpl<Integer> arr = new ArrayListImpl<>();
+		arr.put(2).put(4).put(5).put(6).put(2, 10);
+		arr.delete(1);
+		for (int i = 0; i < arr.getSize(); i++) {
+			System.out.print(arr.get(i) + " ");
+		}
 		
 		HashSetImpl<String> hash = new HashSetImpl<>();
 		hash.put("Apple").put("Wine").put("Party").put("Sweden").put("AAAAA");
